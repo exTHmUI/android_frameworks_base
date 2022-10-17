YuyukoAOSPMod/frameworks_base

base: Add Chinese fine time display
Change-Id: Icff0e5f028a7b8bcb1790865786c51980eb3fb18

packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -119,7 +119,7 @@ public class Clock extends TextView implements
                 R.styleable.Clock,
                 0, 0);
         try {
-            mAmPmStyle = a.getInt(R.styleable.Clock_amPmStyle, AM_PM_STYLE_GONE);
+            mAmPmStyle = a.getInt(R.styleable.Clock_amPmStyle, AM_PM_STYLE_NORMAL);
             mNonAdaptedColor = getCurrentTextColor();
         } finally {
             a.recycle();
@@ -413,7 +413,7 @@ public class Clock extends TextView implements
         } else {
             sdf = mClockFormat;
         }
-        String result = sdf.format(mCalendar.getTime());
+        String result = is24 ? sdf.format(mCalendar.getTime()) : DateFormat.format(format, mCalendar.getTime()).toString();
 
         if (mAmPmStyle != AM_PM_STYLE_NORMAL) {
             int magic1 = result.indexOf(MAGIC1);

exTHmUI/android_frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/policy/Clock.java
@@ -126,7 +126,7 @@ public class Clock extends TextView implements
                 R.styleable.Clock,
                 0, 0);
         try {
-            mAmPmStyle = a.getInt(R.styleable.Clock_amPmStyle, AM_PM_STYLE_GONE);
+            mAmPmStyle = a.getInt(R.styleable.Clock_amPmStyle, AM_PM_STYLE_NORMAL);
             mNonAdaptedColor = getCurrentTextColor();
         } finally {
             a.recycle();
@@ -457,7 +457,7 @@ public class Clock extends TextView implements
             }
             mClockFormat = new SimpleDateFormat(format);
         }
-        String result = mClockFormat.format(mCalendar.getTime());
+        String result = is24 ? mClockFormat.format(mCalendar.getTime()) : DateFormat.format(format, mCalendar.getTime()).toString();
 
         if (mAmPmStyle != AM_PM_STYLE_NORMAL) {
             int magic1 = result.indexOf(MAGIC1);